1deb80bf7e50b6f6461d44cd862424db4e16e78b
15110139 <000big0bang000@gmail.com>
2019-03-29 15:11:28 +0700

    tien

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import * as env from 'dotenv'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware'
 
 async function bootstrap() {
   env.config()
   const PORT = process.env.PORT || 2048
   const app = await NestFactory.create(AppModule)
+  if (process.env.NODE_ENV === 'production') {
+    app.use('/schema', voyagerMiddleware({ endpointUrl: '/graphql' }))
+  }
   await app.listen(PORT)
 
   console.log(`-----------Port: ${PORT}-------------`)
